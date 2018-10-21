6c456e9a4ef4fc04cb378f80f5eb0aa2bb387460
Patryk Spychalski <p.spychalski@ntiinnovations.com>
2018-10-21 16:29:43 +0200

    json config file

--- p-install.py
+++ p-install.py
@@ -1,24 +1,20 @@
 import sys
+import json
 import zipfile
 
 # print(str(sys.argv))
 
 # default values
-zip_path = 'D:\\prestashop_1.6.1.18.zip'
-www_path = 'C:\\xampp\\htdocs'
 
-domain = 'http://presta.com'
-db_server = 'localhost'
-db_name = 'presta'
-db_user = 'root'
-db_password = ''
+configFile = open("config.json", "r")
+configArray = json.loads(configFile.read())
 
 
-zipObj = zipfile.ZipFile(zip_path)
-for file in zipObj.namelist():
-	if file.startswith('prestashop/'):
-		zipObj.extract(file, 'C:\\Users\\gladius882\\test')
-zipObj.close()
+# zipObj = zipfile.ZipFile(zip_path)
+# for file in zipObj.namelist():
+# 	if file.startswith('prestashop/'):
+# 		zipObj.extract(file, 'C:\\Users\\gladius882\\test')
+# zipObj.close()
 
 
 # print(var)
